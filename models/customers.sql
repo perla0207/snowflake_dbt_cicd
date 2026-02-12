c379586a61cdaf32307f7a2c5cfd760e798e0ad8
Ravi Kumar Perla <rperla@evoketechnologies.com>
2026-02-12 22:25:19 +0530

    Added new customer record

--- models/customers.sql
+++ models/customers.sql
@@ -1,9 +1,13 @@
 {{ config(materialized='table') }}
 
 select
-    1 as customer_id,
-    'abc@gmail.com' as email
+  1 as customer_id,
+  'a@abc.com' as email
 union all
 select
-    2 as customer_id,
-    'xyz@gmail.com' as email
+  2 as customer_id,
+  'b@abc.com' as email
+union all
+select
+  3 as customer_id,
+  'c@abc.com' as email
